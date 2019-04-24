a8828cd3571f6221d670c02025a1ca3094ae0687
Dave O'Neill <daveoneill234@gmail.com>
2019-04-23 17:42:32 -0700

    Initial Commit

---
+++ AVFoundationLooper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundationLooper
+//
+//  Created by David O'Neill on 4/23/19.
+//  Copyright © 2019 O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
